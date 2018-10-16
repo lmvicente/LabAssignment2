3160f2a3ed76659845d811beb98ae0d5c4970f7f
lmvicente <leah.m.vicente@gmail.com>
2018-10-16 16:47:19 -0700

    index

---
+++ Assignment2/main.js
@@ -0,0 +1,16 @@
+var Greeter = /** @class */ (function () {
+    function Greeter(first_name, last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+    Greeter.prototype.showGreeting = function () {
+        var banner = document.getElementById('banner');
+        banner.innerHTML = "Hello" + " " + this.first_name + " " + this.last_name + ".";
+    };
+    Greeter.prototype.hideGreeting = function () {
+        var banner = document.getElementById('banner');
+        banner.innerHTML = "r1c2";
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("Leah", "Vicente");
---
+++ Assignment2/main.ts
@@ -0,0 +1,18 @@
+class Greeter {
+
+    constructor(private first_name: string, private last_name: string) {
+    }
+
+    showGreeting() {
+        const banner = document.getElementById('banner');
+        banner.innerHTML = "Hello" + " " + this.first_name + " " + this.last_name + ".";
+    }
+
+    hideGreeting() {
+        const banner = document.getElementById('banner');
+        banner.innerHTML = "r1c2";
+    }
+}
+
+    const greeter = new Greeter("Leah", "Vicente");
+
