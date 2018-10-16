098e57aba9fe8541731b64ce39571fec5f1406cc
lmvicente <leah.m.vicente@gmail.com>
2018-10-16 16:44:04 -0700

    commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var MyFirstProgram = /** @class */ (function () {
+    function MyFirstProgram() {
+        var argArray = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            argArray[_i] = arguments[_i];
+        }
+        this.printArgs(argArray);
+    }
+    MyFirstProgram.prototype.printArgs = function (argArray) {
+        console.log(argArray);
+    };
+    MyFirstProgram.prototype.runFirst = function (callback) {
+        console.log('this method runs first!...');
+    };
+    MyFirstProgram.prototype.rundLast = function () {
+        console.log('this method runs last!');
+    };
+    MyFirstProgram.prototype.runMiddle = function () {
+        console.log('this code run in between the other two functions...');
+    };
+    return MyFirstProgram;
+}());
+var newInstance = new MyFirstProgram("a", "b", "c");
---
+++ main.ts
@@ -0,0 +1,22 @@
+
+
+class MyFirstProgram {
+  constructor(...argArray: Array<any>) {
+    this.printArgs(argArray);
+  }
+  printArgs(argArray) {
+    console.log(argArray);
+  }
+  runFirst(callback: Function) {
+    console.log('this method runs first!...');
+  }
+  rundLast() {
+    console.log('this method runs last!');
+  }
+  runMiddle() {
+    console.log('this code run in between the other two functions...');
+  }
+}
+
+const newInstance = new MyFirstProgram("a", "b", "c");
+
